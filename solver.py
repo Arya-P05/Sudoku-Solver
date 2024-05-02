58563fe1cb820b3a7416164f1f09f686dd8199d9
Arya <arya.patel2354@gmail.com>
2024-05-01 21:23:24 -0400

    print check

--- solver.py
+++ solver.py
@@ -1,3 +1,15 @@
+board = [
+    [7,8,0,4,0,0,1,2,0],
+    [6,0,0,0,7,5,0,0,9],
+    [0,0,0,6,0,1,0,7,8],
+    [0,0,7,0,4,0,2,6,0],
+    [0,0,1,0,5,0,9,3,0],
+    [9,0,4,0,6,0,0,0,5],
+    [0,7,0,3,0,0,0,1,2],
+    [1,2,0,0,0,7,4,0,0],
+    [0,4,9,2,0,6,0,0,7]
+]
+
 def valid_board(board, num, pos):
     for row in range(len(board[0])):
         if board[pos[0]][row] == num and pos[1] != row:
@@ -28,14 +40,20 @@ def find_empty(board):
 def print_board(board):
     for row in range(len(board)):
         if row % 3 == 0 and row != 0:
-            print("- - - - - - - - - - - - - ")
+            print("- - - - - - - - - - - ")
 
         for col in range(len(board[0])):
             if col % 3 == 0 and col != 0:
-                print(" | ", end="")
+                print("| ", end="")
 
             if col == 8:
                 print(board[row][col])
             else:
                 print(str(board[row][col]) + " ", end="")
 
+print()
+print_board(board)
+print("_____________________")
+print()
+print_board(board)
+print()
