6f9303e4ba6776489f0978f92803e7f38dd33e23
Arya <arya.patel2354@gmail.com>
2024-05-01 21:16:13 -0400

    none added

--- solver.py
+++ solver.py
@@ -1,6 +1,24 @@
+
+
 def find_empty(board):
     for row in range(len(board)):
         for col in range(len(board[0])):
             if board[row][col] == 0:
                 return (row, col)
+            
+    return None
+
+def print_board(bo):
+    for i in range(len(bo)):
+        if i % 3 == 0 and i != 0:
+            print("- - - - - - - - - - - - - ")
+
+        for j in range(len(bo[0])):
+            if j % 3 == 0 and j != 0:
+                print(" | ", end="")
+
+            if j == 8:
+                print(bo[i][j])
+            else:
+                print(str(bo[i][j]) + " ", end="")
 
