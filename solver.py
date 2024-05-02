a766b36d9912719b7c0a362579aa89752e6487d3
Arya <arya.patel2354@gmail.com>
2024-05-01 21:20:42 -0400

    valid check

--- solver.py
+++ solver.py
@@ -1,4 +1,21 @@
+def valid_board(board, num, pos):
+    for row in range(len(board[0])):
+        if board[pos[0]][row] == num and pos[1] != row:
+            return False
+    
+    for col in range(len(board)):
+        if board[col][pos[1]] == num and pos[0] != col:
+            return False
+        
+    box_x = pos[1] // 3
+    box_y = pos[0] // 3
 
+    for row in range(box_y * 3, box_y * 3 + 3):
+        for col in range(box_x * 3, box_x * 3 + 3):
+            if board[row][col] == num and (row, col) != pos:
+                return False
+            
+    return True
 
 def find_empty(board):
     for row in range(len(board)):
